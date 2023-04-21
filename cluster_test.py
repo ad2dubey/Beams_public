af8cd603b51a922ff91c128cf04806c763b29054
blackbriar-pwd <blackbriar-pwd@users.noreply.github.com>
2023-04-21 16:42:12 -0400

    Latest working per frame clustering

--- cluster_test.py
+++ cluster_test.py
@@ -11,16 +11,16 @@ import open3d as o3d
 from munkres import Munkres, print_matrix
 import sys
 
-# global constants 
+# global constants wont change
 eps = 0.16
 min_pts = 250
 alpha = 0.25
 L = np.array([10000,10000,10000]) #Limit type1
 L2 = 10000 #Limit type2
 thresh = 5 #threshold for the dsitance above which the distance is made L2
-maxFails = 10 #int(sys.argv[1]) #Maximum number of frames for which a track is checked. Failure after maxFails will lead to removal of the track
+maxFails = 10 #Maximum number of frames for which a track is checked. Failure after maxFails will lead to removal of the track
 
-#Variables for tracks
+#structs monitoring tracks
 global tracks
 global track_status
 global counter
@@ -28,21 +28,8 @@ tracks = []
 track_status = []
 counter = 0
 
-# def preprocess():
-    # df = pd.read_csv('Apr10/zigzagwalk_1.csv')
-    # header = pd.DataFrame([float(x) for x in df.columns]).T #Current Header is meant to be the first row
-    # df.columns = ['timestamp', 'frame' , 'point' , 'X', 'Y', 'Z', 'doppler', 'intensity'] #renaming rows
-    # header.columns = df.columns #Using the same columns of dataframs as the column names for the header.
-    # df = pd.concat([header,df]).reset_index(drop=True) #Concatenating the first row that was previously the header with the rest of the dataframe.
-    # frames = dict(tuple(df.groupby('frame')))
-    # keys = list(frames.keys())
-    # i = 0
-    # for k in keys:
-    #     fr = frames[k]
-    # return fr
-
 def frame_processing(frame):
-    fr =  fra #preprocess() #preprocessing frame
+    fr =  frame
     xyz = fr[['X','Y','Z']].to_numpy()
     xyz[:,2] = 0.0 #Removing the z coordinate data
     labels = dbscan(xyz) #Performs dbscan
@@ -67,7 +54,7 @@ def frame_processing(frame):
     #     continue
     ####*** Need to see what happens if no centroids exist in a frame ***#####
     tracks_update(centroid_array,centroid_doppler,centroid_label)
-
+    
 def dbscan(xyz):
     scaled_points = StandardScaler().fit_transform(xyz)
     print(f"Scaled points are : Max = {max(scaled_points[0])}, Min = {min(scaled_points[0])}")
@@ -76,7 +63,10 @@ def dbscan(xyz):
     model.fit(scaled_points)
     return model.labels_
 
-def tracks_update(centroid_array,centroid_doppler,centroid_label):
+'''
+Centroid doppler and label not being used
+'''
+def tracks_update(centroid_array, centroid_doppler,centroid_label):
 
     global tracks
     global track_status
@@ -97,6 +87,7 @@ def tracks_update(centroid_array,centroid_doppler,centroid_label):
     print(f"Frame {counter} statistics:")
     print(f"tracks length = {len(tracks)}")
     print(f"fr_points length = {len(fr_points)}")
+    print(f"This set points must be returned = {fr_points}?")
 
     #Padding the shorter array with L, where L is a far away coordinate wrt all axes.
     lendiff = len(track_points) - len(fr_points)
@@ -108,6 +99,7 @@ def tracks_update(centroid_array,centroid_doppler,centroid_label):
 
     #Create matrix of distances from the two lists
     matrix = dist_matrix(track_points,fr_points)
+    
     #Applying Hungarian algorithm
     m = Munkres()
     indexes = m.compute(matrix)
@@ -127,11 +119,9 @@ def tracks_update(centroid_array,centroid_doppler,centroid_label):
             #2. Add the cluster from the current frame as a new track.
             add_to_tracks(fr_points[column],column)
     #Delete tracks that are marked for deletion
-    if len(tracks) > 0: 
-        delete_tracks()
-
     #Visualizing
     #visualize()
+    
 
 def dist(p1, p2):
     #Calculating distance between two points. Z-axis is weighted to reduce importance.
@@ -143,8 +133,8 @@ def dist(p1, p2):
 def extract_last_points(arr):
     return [item[-1] for item in arr]
 
-def convert(arr):
-    lister = []
+def convert(arr):   
+    lister = [] 
     for x in arr:
         lister.append(x)
     return lister
@@ -206,7 +196,6 @@ def visualize( ):
     printset = np.vstack([printset,np.array([0,0,0])])
     print("printset is \n", printset)
     pcd_track = o3d.geometry.PointCloud()
-
     pcd_track.points = o3d.utility.Vector3dVector(printset)
     numClusters = len(printlabels)
     colors_track = plt.get_cmap("tab10")(printlabels/ (numClusters if numClusters > 0 else 1))
@@ -218,18 +207,29 @@ def visualize( ):
     print(f"Length of tracks is {len(tracks)}")
     o3d.visualization.draw_geometries([pcd_track], mesh_show_wireframe=True)
 
-if __name__ == '__main__':
-    # if frame start processing, else keep reading
-    df = pd.read_csv('Apr10/zigzagwalk_1.csv')
+
+def preprocess(df):
     header = pd.DataFrame([float(x) for x in df.columns]).T #Current Header is meant to be the first row
     df.columns = ['timestamp', 'frame' , 'point' , 'X', 'Y', 'Z', 'doppler', 'intensity'] #renaming rows
     header.columns = df.columns #Using the same columns of dataframs as the column names for the header.
     df = pd.concat([header,df]).reset_index(drop=True) #Concatenating the first row that was previously the header with the rest of the dataframe.
     frames = dict(tuple(df.groupby('frame')))
+    return frames
+
+if __name__ == '__main__':
+    # if frame start processing, else keep reading
+    df = pd.read_csv('Apr10/walk_stand_1.csv')
+    
+    #open tx-sector files
+
+    frames= preprocess(df)
     keys = list(frames.keys())
     i = 0
     for k in keys:
-        fr = frames[k]
-        frame_processing(fr)
+        current_frame = frames[k]
+        print('******************Frame starts******************')
+        frame_processing(current_frame)
+        ## get the current tracks here
+        print('******************Frame ends******************\n\n')
+    
 
-    # visualize entire thing here
