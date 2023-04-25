cb69857a384323bce835d85cbad246384b8e499a
AwnProject4 <130477927+AwnProject4@users.noreply.github.com>
2023-04-24 21:45:28 -0400

    client socket code for radar station

--- client_soc.py
+++ client_soc.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -6,8 +7,13 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Send a message to the server
 server_address = ('192.168.250.11', 9999)
 message = 'Hello, server!'
-sock.sendto(message.encode(), server_address)
-
+while True:
+    with open("xy.txt",'r+') as fo:
+        if os.path.getsize('xy.txt')!=0:
+            response = fo.readlines()[-1]
+            fo.truncate(0)
+            print(response) 
+            sock.sendto(response.encode(), server_address)
 # Wait for a response from the server
 response, server = sock.recvfrom(4096)
 
